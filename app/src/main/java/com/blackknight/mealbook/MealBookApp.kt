0b029892450464b115a2a332343241744f791b71
vinayagasundar <vinayagasundar93@gmail.com>
2022-02-17 21:58:08 +0800

    (feat) Added Empty landing activity

---
+++ app/src/main/java/com/blackknight/mealbook/MealBookApp.kt
@@ -0,0 +1,7 @@
+package com.blackknight.mealbook
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MealBookApp : Application()
---
+++ app/src/main/java/com/blackknight/mealbook/landing/LandingActivity.kt
@@ -0,0 +1,12 @@
+package com.blackknight.mealbook.landing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blackknight.mealbook.R
+
+class LandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+}
